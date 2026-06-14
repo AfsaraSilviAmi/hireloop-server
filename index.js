497a00336df7e8dd5b01cf94fe5955355ae4758f
Afsara Silvi Ami <afsarasilvi44@gmail.com>
2026-06-14 07:25:35 +0600

    server is updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 5000
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -33,6 +33,9 @@ async function run() {
     const jobCollection = database.collection("jobs")
     const companyCollection = database.collection("companies")
     const userCollection = database.collection("user")
+    const applicationsCollection = database.collection("applications")
+    const planCollection = database.collection("plans")
+    const subscriptionCollection = database.collection("subscriptions")
 
     app.get("/api/users", async(req, res)=>{
       const cursor = userCollection.find()
@@ -53,6 +56,13 @@ async function run() {
         res.send(result)
     })
 
+    app.get("/api/jobs/:id", async(req, res)=>{
+      const id = req.params.id
+    const query = { _id: new ObjectId(id)}
+      const result = await jobCollection.findOne(query)
+      res.send(result)
+    })
+
     app.post("/api/jobs", async(req, res)=>{
         const job = req.body;
         const newJob = {
@@ -63,11 +73,41 @@ async function run() {
         res.send(result)
     })
 
+    //application 
+    app.get("/api/applications", async(req, res)=>{
+      const query = {}
+      if(req.query.applicantId){
+        query.applicantId = req.query.applicantId
+      }
+      if(req.query.jobId){
+        query.jobId = req.query.jobId
+      }
+      const cursor = applicationsCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+    app.post("/api/applications", async(req, res)=>{
+      const application = req.body;
+      const newApplication = {
+        ...application,
+        createdAt: new Date()
+      }
+      const result = await applicationsCollection.insertOne(newApplication)
+      res.send(result)
+    })
+
     //company related apis
     app.get("/api/companies", async(req, res)=>{
       const cursor = companyCollection.find()
-      const result = await cursor.toArray()
-      res.send(result)
+      const companies = await cursor.toArray()
+      for(const company of companies){
+        const filter = {
+          companyId: company._id.toString()
+        }
+        const jobCount = await jobCollection.countDocuments(filter)
+        company.jobCount = jobCount
+      }
+      res.send(companies)
     })
     app.get("/api/my/companies", async(req, res)=>{
        const query = {}
@@ -88,6 +128,50 @@ async function run() {
       }
       const result = await companyCollection.insertOne(newCompany)
       res.send(result)
+    })
+
+    app.patch("/api/companies/:id", async(req, res)=>{
+      const id = req.params.id
+      const updatedCompany = req.body;
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+      $set: {
+       status: updatedCompany.status 
+      },
+    };
+    const result = await companyCollection.updateOne(filter, updateDoc)
+    res.send(result)
+
+    })
+
+    //plans
+
+    app.get("/api/plans", async(req, res)=>{
+      const query = {}
+      if(req.query.plan_id){
+        query.id = req.query.plan_id
+      }
+      const plan = await planCollection.findOne(query)
+      res.send(plan)
+    })
+
+    //subscription
+    app.post("/api/subscriptions", async(req, res)=>{
+        const data = req.body
+        const subsInfo = {
+          ...data,
+          createdAt: new Date()
+        }
+        const result = await subscriptionCollection.insertOne(subsInfo)
+        //update user info
+        const filter = {email: data.email}
+        const updateDocument = {
+      $set: {
+        plan: data.planId,
+      }
+     }
+     const updateResult = await userCollection.updateOne(filter, updateDocument)
+        res.send(updateResult)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
